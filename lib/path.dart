17cbfa7439eb13cdef2cef54b3924535e382565e
Igor Zorzela <igorzorzela@gmail.com>
2022-08-20 12:49:08 -0300

    Readm -> OK

--- lib/path.dart
+++ lib/path.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 
 class MyPath extends StatelessWidget {
-  //const MyPixel({Key? key}) : super(key: key);
-
   final innerColor;
   final outerColor;
   final child;
 
   MyPath({this.innerColor, this.outerColor, this.child, Color? color});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(1.0),
+      padding: const EdgeInsets.all(1),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(6),
         child: Container(
-          padding: EdgeInsets.all(15),
+          padding: EdgeInsets.all(12),
           color: outerColor,
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(5),
+            borderRadius: BorderRadius.circular(10),
             child: Container(
               color: innerColor,
               child: Center(child: child),
